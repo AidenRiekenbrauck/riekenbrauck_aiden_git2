df3fbb402167037b8f76f9067551af91ecbc0d3a
AidenRiekenbrauck <AidenRiekenbrauck>
2018-02-15 10:34:18 -0500

    kil me

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 (()=>{
   console.log('fired! kill me')
+
+var iconSet = document.querySelector('svg');
+
+function logThisIcon(){
+  console.log('clicked on this icon: ', this.id);
+
+this.style.opacity = 0.5;
+}
+
+iconSet.forEach(icon => icon.addEventListener('click', logThisIcon));
+
 })();
